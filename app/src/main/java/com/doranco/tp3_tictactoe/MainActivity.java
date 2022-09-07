f197794de6c132e8420d895c88a0e080208bf750
Nadine Doranco <havanueva@gmail.com>
2022-09-07 09:31:57 +0200

    tic tac toe

--- app/src/main/java/com/doranco/tp3_tictactoe/MainActivity.java
+++ app/src/main/java/com/doranco/tp3_tictactoe/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         grid = new String[9];
         buttons = new Button[9];
         winnings = new String[8];
